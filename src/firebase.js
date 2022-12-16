66b3d377a9b3728adb1ba2deb27b919df2f19ab8
crt <skyrocket007@hanmail.net>
2022-12-17 02:29:49 +0900

    second commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// firebase 불러오는 코드
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0uv2R9iDJt1sD29M_d7Jb4RiZVd0rHk8",
+
+  authDomain: "clone-d2b94.firebaseapp.com",
+
+  projectId: "clone-d2b94",
+
+  storageBucket: "clone-d2b94.appspot.com",
+
+  messagingSenderId: "244887699921",
+
+  appId: "1:244887699921:web:f5359ec5c43786032e20f0",
+
+  measurementId: "G-W1Y0S704ZZ",
+};
