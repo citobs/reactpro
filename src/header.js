9f3d50b60e6c6f1e4d5b07dae3dd067fbcdc2a16
crt <skyrocket007@hanmail.net>
2022-12-17 02:58:34 +0900

    header 제작중

---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+function Header(props) {
+  <div className='header'>
+    <img className='Header' alt='' />
+  </div>;
+}
+
+export default Header;
