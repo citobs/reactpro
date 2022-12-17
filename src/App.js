17c0930b804e4918d181ef3007c5b1b2fb5eedd9
crt <skyrocket007@hanmail.net>
2022-12-17 20:36:22 +0900

    본문(home.js)작성시작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className='App'>
       <Header />
-
-      <h1>입문</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home-container'>
+        <img
+          className='home_image'
+          src='http://drive.google.com/uc?export=view&id=1Y56DRwIRq3dHlJqzYCNaVMXzgvLkwUJO'
+          alt='와카리나잇!'
+        />
+      </div>
+      <h1>본문입니다</h1>
+    </div>
+  );
+}
+export default Home;
