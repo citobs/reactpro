c32a07f345103acc8355152430eda1e9b61c1538
crt <skyrocket007@hanmail.net>
2022-12-19 04:14:13 +0900

    로그인기능완성/firebase연동시작

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { useStateValue } from "./StateProvider";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className='login'>
       <Link path='/'>
@@ -12,6 +21,33 @@ function Login() {
           alt=''
         />
       </Link>
+
+      <div className='login_container'>
+        <h1>로그인</h1>
+        <form>
+          <h5> 이메일 </h5>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type='text'
+          />
+          <h5> 비밀번호 </h5>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+          />
+          <button onClick={signIn} className='login_signInButton'>
+            {" "}
+            로그인하기
+          </button>
+        </form>
+        <p> 이용약관 동의하십니까?</p>
+        <button onClick={register} className='login_registerButton'>
+          {" "}
+          회원가입
+        </button>
+      </div>
     </div>
   );
 }
