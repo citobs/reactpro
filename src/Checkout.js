80504a529c9f8d48a95ae55d71b844cc4a254f6f
crt <skyrocket007@hanmail.net>
2022-12-19 01:24:46 +0900

    장바구니리스트출력가능/제거기능추가시작

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
       <div className='checkout_left'>
@@ -15,8 +18,14 @@ function Checkout() {
 
         <div>
           <h2 className='checkout_title'>장바구니입니다.</h2>
-
-          <CheckoutProduct />
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+            />
+          ))}
         </div>
       </div>
       <div className='checkout_right'>
